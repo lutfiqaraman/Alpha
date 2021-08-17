73a1d60b95f6783eb0a1fc37987f7a64fd4d0633
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-17 06:20:46 +0300

    creating dbcontext of the project

--- Alpha.EntityFramework/AlphaDbContext.cs
+++ Alpha.EntityFramework/AlphaDbContext.cs
@@ -4,5 +4,13 @@ namespace Alpha.EntityFramework
 {
     public class AlphaDbContext : DbContext
     {
+
+        //Add DbSet properties for your entities...
+
+        public AlphaDbContext(DbContextOptions<AlphaDbContext> options)
+            :base(options)
+        {
+
+        }
     }
 }
