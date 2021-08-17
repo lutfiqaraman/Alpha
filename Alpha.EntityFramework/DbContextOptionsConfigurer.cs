ffd89fa25aa3b379f30369f29ab7c8a5a6a16adb
lutfiqaraman <lutfiqaraman@gmail.com>
2021-08-17 21:55:14 +0300

    Create a configuration of dbcontext

---
+++ Alpha.EntityFramework/DbContextOptionsConfigurer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alpha.EntityFramework
+{
+    public static class DbContextOptionsConfigurer
+    {
+        public static void Configure(DbContextOptionsBuilder<AlphaDbContext> dbContextOptions, 
+            string connectionString)
+        {
+            dbContextOptions.UseSqlServer(connectionString);
+        }
+    }
+}
